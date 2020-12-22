a5b3d7ce53b3f910b71076e2cc0742c52642055a
Admin <tru.elli.yes@gmail.com>
2020-12-22 17:26:39 +0300

    L4

---
+++ Lab_4/src/JImageDisplay.java
@@ -0,0 +1,32 @@
+import javax.swing.JComponent;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class JImageDisplay extends JComponent {
+    private final BufferedImage image;
+
+    public JImageDisplay(int w, int h){
+        image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Dimension dimension = new Dimension(w, h);
+
+        super.setPreferredSize(dimension);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawImage (image, 0, 0, image.getWidth(), image.getHeight(), null);
+    }
+
+    public void clearImage() {
+        Graphics2D imageGraphics = image.createGraphics();
+        imageGraphics.setColor(new Color(0, 0, 0));
+
+        imageGraphics.fillRect(0, 0, image.getWidth(), image.getHeight());
+    }
+
+    public void drawPixel (int x, int y, int rgbColor){
+        image.setRGB(x, y, rgbColor);
+    }
+}
