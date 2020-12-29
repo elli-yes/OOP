ecca7e0460221b31a5d661181fc868b3f27d3957
Admin <tru.elli.yes@gmail.com>
2020-12-29 19:02:28 +0300

    ts

--- Tasks/src/Tasks.java
+++ Tasks/src/Tasks.java
@@ -1,5 +1,5 @@
-//import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
-import java.math.*;
+import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -208,7 +208,7 @@ class Module3 {
         ArrayList<Integer> dividers = new ArrayList<>();
 
         for (int i = 1; i < num; i++) {
-            if (num % i == 0) dividers.add(i);
+            if (num / i == 0) dividers.add(i);
         }
 
         int sum = 0;
@@ -613,7 +613,7 @@ class Module5 {
         }
 
         return false;
-    } 
+    }
 
     public static boolean canComplete(String str1, String str2) {
         char[] chars = str1.toCharArray();
@@ -1058,9 +1058,8 @@ class Module5 {
     public static String getSha256Hash(String str) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] text = digest.digest(str.getBytes(StandardCharsets.UTF_8));
-        String aa = new String(text, StandardCharsets.UTF_8);
-         return String.format("%040x", new BigInteger(1, aa.getBytes()));
-        
+
+        return HexBin.encode(text).toLowerCase();
     }
 
     public static String correctTitle(String str) {
@@ -1122,39 +1121,435 @@ class Module5 {
     }
 }
 
+class Module6 {
+    public static int bell(int n) {
+        int[][] bellTriangle = new int[n+1][n+1];
+        bellTriangle[0][0] = 1;
 
+        for (int i=1; i<=n; i++) {
+            bellTriangle[i][0] = bellTriangle[i-1][i-1];
+
+            for (int j=1; j<=i; j++) {
+                bellTriangle[i][j] = bellTriangle[i-1][j-1] + bellTriangle[i][j-1];
+            }
+        }
+
+        return bellTriangle[n][0];
+    }
+
+    public static String translateWord(String word) {
+        String result = word;
+
+        if (String.valueOf(result.charAt(0)).toLowerCase().matches("[aeiouy]")) {
+            result += "yay";
+        } else {
+            result = result.toLowerCase();
+            String newWord = result.split("[aeiouy]")[0];
+            result = result.replaceFirst(newWord,"") + newWord + "ay";
+            result = String.valueOf(result.charAt(0)).toUpperCase() + result.substring(1);
+        }
+
+        return result;
+    }
+
+    public static String translateSentence(String str) {
+        String[] tokens = str.split(" ");
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (String.valueOf(tokens[i].charAt(0)).toLowerCase().matches("[aeiouy]")) {
+                if (String.valueOf(tokens[i].charAt(tokens[i].length() - 1)).matches("[!?.,:;]")) {
+                    tokens[i] = tokens[i].substring(0, tokens[i].length() - 1) + "yay" + tokens[i].charAt(tokens[i].length() - 1);
+                } else {
+                    tokens[i] += "yay";
+                }
+            } else {
+                if (String.valueOf(tokens[i].charAt(0)).matches("[QWRTPSDFGHJKLZXCVBNM]")) {
+                    if (String.valueOf(tokens[i].charAt(tokens[i].length() - 1)).matches("[!?.,:;]")) {
+                        char mark = tokens[i].charAt(tokens[i].length() - 1);
+                        tokens[i] = tokens[i].substring(0, tokens[i].length() - 1);
+                        tokens[i] = tokens[i].toLowerCase();
+                        String newWord = tokens[i].split("[aeiouy]")[0];
+                        tokens[i] = tokens[i].replaceFirst(newWord,"") + newWord + "ay";
+                        tokens[i] = String.valueOf(tokens[i].charAt(0)).toUpperCase() + tokens[i].substring(1) + mark;
+                    } else {
+                        tokens[i] = tokens[i].toLowerCase();
+                        String newWord = tokens[i].split("[aeiouy]")[0];
+                        tokens[i] = tokens[i].replaceFirst(newWord,"") + newWord + "ay";
+                        tokens[i] = String.valueOf(tokens[i].charAt(0)).toUpperCase() + tokens[i].substring(1);
+                    }
+                } else {
+                    if (String.valueOf(tokens[i].charAt(tokens[i].length() - 1)).matches("[!?.,:;]")) {
+                        char mark = tokens[i].charAt(tokens[i].length() - 1);
+                        tokens[i] = tokens[i].substring(0, tokens[i].length() - 1);
+                        tokens[i] = tokens[i].toLowerCase();
+                        String newWord = tokens[i].split("[aeiouy]")[0];
+                        tokens[i] = tokens[i].replaceFirst(newWord,"") + newWord + "ay";
+                        tokens[i] = tokens[i] + mark;
+                    } else {
+                        tokens[i] = tokens[i].toLowerCase();
+                        String newWord = tokens[i].split("[aeiouy]")[0];
+                        tokens[i] = tokens[i].replaceFirst(newWord,"") + newWord + "ay";
+                    }
+                }
+            }
+        }
+
+        return String.join(" ", tokens);
+    }
+
+    public static boolean validColor(String str) {
+        if (!str.startsWith("rgb") && !str.startsWith("rgba")) {
+            return false;
+        }
+
+        String[] numbers = str.split("\\(")[1].split(",");
+        numbers[numbers.length - 1] = numbers[numbers.length - 1].substring(0, numbers[numbers.length - 1].length() - 1);
+
+        if (str.startsWith("rgb") && !str.startsWith("rgba")) {
+            if (str.contains(".")) {
+                return false;
+            }
+
+            for (int i = 0; i < numbers.length; i ++) {
+                if (numbers[i].trim().equals("")) {
+                    return false;
+                }
+
+                int num = Integer.parseInt(numbers[i].trim());
+
+                if (!(num >= 0 && num <= 255)) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < numbers.length - 1; i ++) {
+                if (numbers[i].trim().equals("")) {
+                    return false;
+                }
+
+                int num = Integer.parseInt(numbers[i].trim());
+
+                if (!(num >= 0 && num <= 255)) {
+                    return false;
+                }
+            }
+
+            if (numbers[3].trim().equals("")) return false;
+
+            double num = Double.parseDouble(numbers[3].trim());
+
+            return num >= 0 && num <= 1;
+        }
+
+        return true;
+    }
+
+    public static String stripUrlParams(String url, String ...paramsToStrip) {
+        String str = "";
+
+        if (!url.contains("?"))
+            return url;
+        else {
+            str = url.substring(url.indexOf("?") + 1);
+            url = url.substring(0, url.indexOf("?") + 1);
+        }
+
+        char[] params = str.toCharArray();
+        StringBuilder print = new StringBuilder();
+
+        for (char param : params) {
+            if (Character.isLetter(param))
+                if (!(print.toString().contains(String.valueOf(param)))) {
+                    if (paramsToStrip.length > 0) {
+                        for (String arg : paramsToStrip) {
+                            if (!(arg.contains(String.valueOf(param))))
+                                print.append(str, str.lastIndexOf(param), str.lastIndexOf(param) + 3).append("&");
+                        }
+                    }
+                    else
+                        print.append(str, str.lastIndexOf(param), str.lastIndexOf(param) + 3).append("&");
+                }
+        }
+
+        return url + print.substring(0, print.length()-1);
+    }
+
+    public static ArrayList<String> getHashTags(String str){
+        String[] tokens = str.toLowerCase().split(" ");
+        ArrayList<String> hashtags = new ArrayList<>();
+
+        while (hashtags.size() < 3) {
+            double maxLength = Double.NEGATIVE_INFINITY;
+            String word = "";
+            int idx = 0;
+
+            try {
+                for (int i = 0; i < tokens.length; i++) {
+                    if (tokens[i].length() > maxLength) {
+                        maxLength = tokens[i].length();
+                        word = tokens[i];
+                        idx = i;
+                    }
+                }
+
+                if (String.valueOf(word.charAt(word.length() - 1)).matches("[!?.,;:]")) {
+                    hashtags.add("#" + word.substring(0, word.length() - 1));
+                } else {
+                    hashtags.add("#" + word);
+                }
+                tokens[idx] = "";
+            } catch (StringIndexOutOfBoundsException e) {
+                return hashtags;
+            }
+        }
+
+        return hashtags;
+    }
+
+    public static int ulam (int n){
+        int[] arr = new int[n];
+        arr[0]=1;
+        arr[1]=2;
+        int len=2, next=3;
+
+        while (next < Integer.MAX_VALUE && len < n){
+            int count = 0;
+
+            for (int i = 0; i < len; i++){
+                for (int j = len-1; j > i; j--){
+                    if (arr[i] + arr[j] == next && arr[i] != arr[j])
+                        count++;
+                    else if (count > 1)
+                        break;
+                }
+
+                if (count > 1)
+                    break;
+            }
+            if (count == 1) {
+                arr[len] = next;
+                len++;
+            }
+            next++;
+        }
+        return arr[n-1];
+    }
+
+    public static String longestNonrepeatingSubstring(String str){
+        String substr = "";
+        char [] chars = str.toCharArray();
+        StringBuilder builder = new StringBuilder();
+
+        for (char c : chars) {
+            if (!builder.toString().contains(String.valueOf(c)))
+                builder.append(c);
+            else {
+                if (builder.length() > substr.length()) {
+                    substr = builder.toString();
+                }
+                builder = new StringBuilder("" + c);
+            }
+        }
+
+        str = builder.toString();
+
+        if (str.length() > substr.length())
+            substr = str;
+
+        return substr;
+    }
+
+    public static String convertToRoman (int num){
+        StringBuilder roman = new StringBuilder();
+
+        if (num < 1 || num > 3999)
+            return "Введите число поменьше. ";
+
+        while (num >= 1000) {
+            roman.append("M");
+            num -= 1000;
+        }
+
+        while (num >= 900) {
+            roman.append("CM");
+            num -= 900;
+        }
+
+        while (num >= 500) {
+            roman.append("D");
+            num -= 500;
+        }
+
+        while (num >= 400) {
+            roman.append("CD");
+            num -= 400;
+        }
+
+        while (num >= 100) {
+            roman.append("C");
+            num -= 100;
+        }
+
+        while (num >= 90) {
+            roman.append("XC");
+            num -= 90;
+        }
+
+        while (num >= 50) {
+            roman.append("L");
+            num -= 50;
+        }
+
+        while (num >= 40) {
+            roman.append("XL");
+            num -= 40;
+        }
+
+        while (num >= 10) {
+            roman.append("X");
+            num -= 10;
+        }
+
+        while (num >= 9) {
+            roman.append("IX");
+            num -= 9;
+        }
+
+        while (num >= 5) {
+            roman.append("V");
+            num -= 5;
+        }
+
+        while (num >= 4) {
+            roman.append("IV");
+            num -= 4;
+        }
+
+        while (num >= 1) {
+            roman.append("I");
+            num -= 1;
+        }
+
+        return roman.toString();
+    }
+
+    public static boolean formula(String formula){
+        String[] tokens = formula.split(" ");
+        int ans = 0;
+        int expectedResult = 0;
+
+        if (!Character.isDigit(tokens[0].charAt(0))) return false;
+        else ans = Integer.parseInt(tokens[0]);
+
+        int i = 1;
+
+        while (!tokens[i].equals("=")) {
+            if (tokens[i].equals("+")){
+                ans += Integer.parseInt(tokens[i + 1]);
+            }
+            if (tokens[i].equals("-")){
+                ans -= Integer.parseInt(tokens[i + 1]);
+            }
+            if (tokens[i].equals("*")){
+                ans *= Integer.parseInt(tokens[i + 1]);
+            }
+            if (tokens[i].equals("/")){
+                ans /= Integer.parseInt(tokens[i + 1]);
+            }
+
+            i += 2;
+        }
+
+        i = formula.indexOf('=');
+        String check = formula.substring(i + 1);
+
+        if (check.contains("=")) return false;
+        else expectedResult = Integer.parseInt(tokens[tokens.length - 1]);
+
+        return ans == expectedResult;
+    }
+
+    public static boolean palindromedescendant(int num){
+        boolean isDescendant = false;
+        StringBuffer buffer1 =new StringBuffer(num);
+        StringBuffer buffer2 =new StringBuffer(num);
+
+        if (buffer1.length() % 2 != 0)
+            return false;
+        else {
+            while (!isDescendant){
+                if(buffer1 != buffer1.reverse()){
+                    for(int i = 0; i < buffer1.length(); i += 2){
+                        int a = Integer.parseInt(String.valueOf(buffer1.charAt(i)));
+                        int b = Integer.parseInt(String.valueOf(buffer1.charAt(i + 1)));
+                        buffer2.append(a + b);
+                    }
+                }
+                else
+                    isDescendant = true;
+            }
+        }
+
+        return isDescendant;
+    }
+}
+
+class Tasks {
+    public static void log(String str) {
+        System.out.println(str);
+    }
+
+    public static void log(double str) {
+        System.out.println(str);
+    }
+
+    public static void log(boolean str) {
+        System.out.println(str);
+    }
+
+    public static void log(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void log(String[] str) {
+        System.out.println(String.join(" ", str));
+    }
+
+    public static void log(ArrayList<String> arr) {
+        System.out.println(Arrays.toString(arr.toArray()));
+    }
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        // log("Module 1");
-        // log("===================================");
-        // log(Module1.remainder(3, 5));
-        // log(Module1.triArea(5, 4));
-        // log(Module1.animals(3, 5, 2));
-        // log(Module1.profitableGamble(0.8, 3, 10));
-        // log(Module1.operation(25, 13, 12));
-        // log(Module1.ctoa('A'));
-        // log(Module1.addUpTo(10));
-        // log(Module1.nextEdge(13, 25));
-        // log(Module1.sumOfCubes(new double[]{10, 20, 30}));
-        // log(Module1.abcmath(10, 3, 15));
-        // log("Module 2");
-        // log("===================================");
-        // log(Module2.repeat("apple", 4));
-        // log(Module2.differenceMaxMin(new int[]{10, 5, 4, 30, -1}));
-        // log(Module2.isAvgWhole(new int[]{3, 5, 10, 14, 8}));
-        // log(Module2.cumulativeSum(new int[]{3, 4, 1, 5, 7, 8}));
-        // log(Module2.getDecimalPlaces("3.5477"));
-        // log(Module2.Fibonacci(7));
-        // log(Module2.isValid("32454"));
-        // log(Module2.isStrangePair("", ""));
-        // log(Module2.isPrefix("automation", "auto-"));
-        // log(Module2.isSuffix("arachnophobia", "-phobia"));
-        // log(Module2.boxSeq(4));
+        log("Module 1");
+        log("===================================");
+        log(Module1.remainder(3, 5));
+        log(Module1.triArea(5, 4));
+        log(Module1.animals(3, 5, 2));
+        log(Module1.profitableGamble(0.8, 3, 10));
+        log(Module1.operation(25, 13, 12));
+        log(Module1.ctoa('A'));
+        log(Module1.addUpTo(10));
+        log(Module1.nextEdge(13, 25));
+        log(Module1.sumOfCubes(new double[]{10, 20, 30}));
+        log(Module1.abcmath(10, 3, 15));
+        log("Module 2");
+        log("===================================");
+        log(Module2.repeat("apple", 4));
+        log(Module2.differenceMaxMin(new int[]{10, 5, 4, 30, -1}));
+        log(Module2.isAvgWhole(new int[]{3, 5, 10, 14, 8}));
+        log(Module2.cumulativeSum(new int[]{3, 4, 1, 5, 7, 8}));
+        log(Module2.getDecimalPlaces("3.5477"));
+        log(Module2.Fibonacci(7));
+        log(Module2.isValid("32454"));
+        log(Module2.isStrangePair("", ""));
+        log(Module2.isPrefix("automation", "auto-"));
+        log(Module2.isSuffix("arachnophobia", "-phobia"));
+        log(Module2.boxSeq(4));
         log("Module 3");
         log("===================================");
         log(Module3.solutions(1, 0, -1));
         log(Module3.findZip("all zip files are zipped"));
-        log(Module3.checkPerfect(27));
+        log(Module3.checkPerfect(6));
         log(Module3.flipEndChars("Cat, dog, and mouse."));
         log(Module3.isValidHexCode("#cde3f5"));
         log(Module3.same(new int[]{1, 3, 4, 4, 4}, new int[]{2, 5, 7}));
@@ -1186,21 +1581,21 @@ class Module5 {
         log(Module5.validateCard(1234567890123452L));
         log(Module5.numToEng(18));
         log(Module5.numToRus(18));
-       log(Module5.getSha256Hash("password123"));
+        log(Module5.getSha256Hash("password123"));
         log(Module5.correctTitle("TYRION LANNISTER, HAND OF THE QUEEN."));
         log(Module5.haxLattice(91));
-    //     log("Module 6");
-    //     log("===================================");
-    //     log(Module6.bell(5));
-    //     log(Module6.translateWord("Do"));
-    //     log(Module6.translateSentence("I like to eat honey waffles."));
-    //     log(Module6.validColor("rgba(0,0,0,0.123456789)"));
-    //     log(Module6.stripUrlParams("https://edabit.com", "b"));
-    //     log(Module6.getHashTags("Visualizing Science"));
-    //     log(Module6.ulam(206));
-    //     log(Module6.longestNonrepeatingSubstring("abcabcbb"));
-    //     log(Module6.convertToRoman(16));
-    //     log(Module6.formula("6 * 4 = 24"));
-    //     log(Module6.palindromedescendant(11211230));
+        log("Module 6");
+        log("===================================");
+        log(Module6.bell(5));
+        log(Module6.translateWord("Do"));
+        log(Module6.translateSentence("I like to eat honey waffles."));
+        log(Module6.validColor("rgba(0,0,0,0.123456789)"));
+        log(Module6.stripUrlParams("https://edabit.com", "b"));
+        log(Module6.getHashTags("Visualizing Science"));
+        log(Module6.ulam(206));
+        log(Module6.longestNonrepeatingSubstring("abcabcbb"));
+        log(Module6.convertToRoman(16));
+        log(Module6.formula("6 * 4 = 24"));
+        log(Module6.palindromedescendant(11211230));
     }
 }
